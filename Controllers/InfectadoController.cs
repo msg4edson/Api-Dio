26b105247be723d827548c4f9f0c0e591410a0a2
msg4edson <msg.edson@gmail.com>
2021-06-23 23:09:14 -0300

    first commit

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,39 @@
+using Api_Dio.Data.Collections;
+using Api_Dio.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Api_Dio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
